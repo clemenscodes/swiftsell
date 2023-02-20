f397b08f8ddccbf48796321b7cedd869fe5f959e
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-20 10:58:37 +0100

    chore(workspace): update nx

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,11 @@ import type { StorybookConfig } from '@storybook/core-common';
 export const rootMain: StorybookConfig = {
     core: { builder: 'webpack5', disableTelemetry: true },
     stories: [],
+    addons: [
+        '@storybook/addon-essentials',
+        '@storybook/addon-links',
+        '@storybook/addon-controls',
+    ],
     // webpackFinal: async (config, { configType }) => {
     //   // Make whatever fine-grained changes you need that should apply to all storybook configs
 
