8513373ccadcc585877d632384029bc6e04c7a0b
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-20 15:45:39 +0100

    chore(web): test

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,11 +1,35 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../global.css';
-import { usePrefixStore } from '@shared';
+import { usePrefixStore, PrefixState } from '@shared';
 
 function CustomApp({ Component, pageProps }: AppProps) {
-    usePrefixStore((state) => state.setPrefix('shop.swfitsell.de'));
-    console.log('ran setprefix');
+    usePrefixStore.setState(
+        ((domain = 'shop.swiftsell.de') => {
+            const state: Partial<PrefixState> = {
+                prefix: '',
+            };
+            if (typeof window !== 'undefined') {
+                const hostname = window.location.hostname;
+                const devDomain = `dev.${domain}`;
+                const prodCDN = `https://static.${domain}/public`;
+                const devCDN = `https://dev.static.${domain}/public`;
+                const devCloudRunURL = 'web-mcepb6criq-ew.a.run.app';
+                const prodCloudRunURL = 'web-ow3benvyza-ew.a.run.app';
+
+                if (hostname === devCloudRunURL || hostname === devDomain) {
+                    state.prefix = devCDN;
+                } else if (
+                    hostname === prodCloudRunURL ||
+                    hostname === domain
+                ) {
+                    state.prefix = prodCDN;
+                }
+                console.log('running with window', { hostname });
+            }
+            return state;
+        })()
+    );
     const PREFIX = usePrefixStore((state) => state.prefix);
     console.log({ PREFIX });
     return (
--- libs/shared/src/store/usePrefixStore.tsx
+++ libs/shared/src/store/usePrefixStore.tsx
@@ -1,33 +1,9 @@
 import { create } from 'zustand';
 
-interface PrefixState {
+export interface PrefixState {
     prefix: string;
-    setPrefix: (domain: string) => void;
 }
 
-export const usePrefixStore = create<PrefixState>((set) => ({
+export const usePrefixStore = create<PrefixState>(() => ({
     prefix: '',
-    setPrefix: (domain: string) => {
-        set((state) => {
-            if (typeof window !== 'undefined') {
-                const hostname = window.location.hostname;
-                const devDomain = `dev.${domain}`;
-                const prodCDN = `https://static.${domain}/public`;
-                const devCDN = `https://dev.static.${domain}/public`;
-                const devCloudRunURL = 'web-mcepb6criq-ew.a.run.app';
-                const prodCloudRunURL = 'web-ow3benvyza-ew.a.run.app';
-
-                if (hostname === devCloudRunURL || hostname === devDomain) {
-                    state.prefix = devCDN;
-                } else if (
-                    hostname === prodCloudRunURL ||
-                    hostname === domain
-                ) {
-                    state.prefix = prodCDN;
-                }
-                console.log('running with window', { hostname });
-            }
-            return state;
-        });
-    },
 }));
