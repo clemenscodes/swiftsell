508bd8865f9e84db29cbfebade4ad2f3c12279a9
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-22 18:14:33 +0100

    test(web): test

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,7 +7,7 @@ const appName = 'shop';
 const base = `${appName}.${apexDomain}`;
 const prodDomain = `${protocol}://${base}`;
 const devDomain = `${protocol}://dev.${base}`;
-const staticBase = `static.${prodDomain}`;
+const staticBase = `static.${base}`;
 const devCDN = `dev.${staticBase}`;
 const prodCDN = `${staticBase}`;
 const isCloudRunProd = process.env.NEXT_PUBLIC_PROJECT_TYPE === 'production';
