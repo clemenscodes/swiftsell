7c4d65331f69157daf31b1564dda53751f5d7f13
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-25 18:45:34 +0100

    fix(web): add cors headers to requests to CDN

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -73,6 +73,26 @@ const nextConfig = {
         // See: https://github.com/gregberge/svgr
         svgr: true,
     },
+    async headers() {
+        const prefix = assetPrefix ? assetPrefix : '';
+        return [
+            {
+                source: `${prefix}/:path*`,
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                    },
+                ],
+            },
+        ];
+    },
     webpack(config) {
         config.module.rules.push({
             test: /index\.(js|mjs|jsx|ts|tsx)$/,
