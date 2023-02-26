052ac68a2bdabaf02319f2faa619da852a33dc92
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-26 20:54:40 +0100

    test(web): test

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,13 +1,18 @@
 import '../global.css';
 import { siteConfig } from '@config';
-import { useSupabaseClient } from '@hooks';
 import { Roboto_Condensed as Font } from '@next/font/google';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { Session } from '@supabase/auth-helpers-react';
 import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import Script from 'next/script';
+import { useState } from 'react';
 
+const Header = dynamic(() => import('@shared').then((mod) => mod.Header));
+const TailwindIndicator = dynamic(() =>
+    import('@shared').then((mod) => mod.TailwindIndicator)
+);
 const SessionContextProvider = dynamic(() =>
     import('@supabase/auth-helpers-react').then(
         (mod) => mod.SessionContextProvider
@@ -15,10 +20,6 @@ const SessionContextProvider = dynamic(() =>
 );
 const ThemeProvider = dynamic(() =>
     import('next-themes').then((mod) => mod.ThemeProvider)
-);
-const Header = dynamic(() => import('@shared').then((mod) => mod.Header));
-const TailwindIndicator = dynamic(() =>
-    import('@shared').then((mod) => mod.TailwindIndicator)
 );
 
 const fontSans = Font({
@@ -34,7 +35,7 @@ const App: React.FC<AppProps<{ initialSession: Session }>> = ({
     Component,
     pageProps,
 }) => {
-    const { supabaseClient } = useSupabaseClient();
+    const [supabase] = useState(() => createBrowserSupabaseClient());
     return (
         <>
             <Head>
@@ -44,46 +45,25 @@ const App: React.FC<AppProps<{ initialSession: Session }>> = ({
                     content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5,user-scalable=yes,viewport-fit=cover'
                 />
             </Head>
-            {supabaseClient ? (
-                <>
-                    <SessionContextProvider
-                        supabaseClient={supabaseClient}
-                        initialSession={pageProps.initialSession}
-                    >
-                        <ThemeProvider
-                            attribute='class'
-                            defaultTheme='dark'
-                            enableSystem
-                        >
-                            <main className={`${fontSans.variable} font-sans`}>
-                                <Header />
-                                <Component
-                                    className='container flex-1'
-                                    {...pageProps}
-                                />
-                                <TailwindIndicator />
-                            </main>
-                        </ThemeProvider>
-                    </SessionContextProvider>
-                </>
-            ) : (
-                <>
-                    <ThemeProvider
-                        attribute='class'
-                        defaultTheme='dark'
-                        enableSystem
-                    >
-                        <main className={`${fontSans.variable} font-sans`}>
-                            <Header />
-                            <Component
-                                className='container flex-1'
-                                {...pageProps}
-                            />
-                            <TailwindIndicator />
-                        </main>
-                    </ThemeProvider>
-                </>
-            )}
+            <SessionContextProvider
+                supabaseClient={supabase}
+                initialSession={pageProps.initialSession}
+            >
+                <ThemeProvider
+                    attribute='class'
+                    defaultTheme='dark'
+                    enableSystem
+                >
+                    <main className={`${fontSans.variable} font-sans`}>
+                        <Header />
+                        <Component
+                            className='container flex-1'
+                            {...pageProps}
+                        />
+                        <TailwindIndicator />
+                    </main>
+                </ThemeProvider>
+            </SessionContextProvider>
             <Script src={`/__ENV.js`}></Script>
         </>
     );
--- libs/shared/src/components/loader/loader.tsx
+++ libs/shared/src/components/loader/loader.tsx
@@ -1,13 +1,18 @@
 import { cn } from '@styles';
+import { Triangle } from 'react-loader-spinner';
 
 /* eslint-disable-next-line */
 export interface LoaderProps {}
 
 export const Loader: React.FC<LoaderProps> = ({ ...props }) => {
     return (
-        <div className={cn([])}>
-            <h1>Welcome to Loader!</h1>
-        </div>
+        <Triangle
+            height='80'
+            width='80'
+            ariaLabel='triangle-loading'
+            wrapperClass={cn()}
+            visible={true}
+        />
     );
 };
 
