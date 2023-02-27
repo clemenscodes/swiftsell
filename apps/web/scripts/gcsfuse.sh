7475d1604c2d7fed3449763ee78fe5e5e01afef6
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-27 15:54:04 +0100

    test(web): test

--- apps/web/scripts/gcsfuse.sh
+++ apps/web/scripts/gcsfuse.sh
@@ -28,8 +28,7 @@ mount_google_cloud_storage() {
         exec gcsfuse --key-file="$GOOGLE_APPLICATION_CREDENTIALS" --foreground --debug_gcs "$BUCKET" "$MNT_DIR" &
     else
         echo "Mounting in Cloud Run..."
-        exec gcsfuse --debug_fuse_errors --debug_fs --debug_fuse --debug_http --debug_gcs --log-file="$LOG_FILE" "$BUCKET" "$MNT_DIR" &
-        sleep 2
+        exec gcsfuse --foreground --debug_fuse_errors --debug_fs --debug_fuse --debug_http --debug_gcs --log-file="$LOG_FILE" "$BUCKET" "$MNT_DIR" &
         cat "$LOG_FILE"
     fi
     echo "Mounting completed."
