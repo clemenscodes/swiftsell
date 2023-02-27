d2410bc3796622e3dab38e0c93bd5ac33c115c9d
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-27 13:16:48 +0100

    test(web): test

--- apps/web/infra/development/variables.tf
+++ apps/web/infra/development/variables.tf
@@ -43,7 +43,7 @@ variable "cloud_run_region" {
 variable "cloud_run_service_name" {
   description = "The name for the Cloud Run service"
   type        = string
-  default     = "web"
+  default     = "swiftsell"
 }
 
 variable "cloud_run_subdomain" {
