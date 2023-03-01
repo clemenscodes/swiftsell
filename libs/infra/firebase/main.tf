ea496ba13251eecca318533c49bf45bb5776c46e
Clemens Horn <clemens.horn@mni.thm.de>
2023-03-01 21:26:51 +0100

    test(web): test

--- libs/infra/firebase/main.tf
+++ libs/infra/firebase/main.tf
@@ -7,86 +7,86 @@ data "google_project" "project" {
   project_id = var.project_id
 }
 
-# resource "google_firebase_project" "default" {
-#   provider = google-beta
-#   project  = data.google_project.project.project_id
+resource "google_firebase_project" "default" {
+  provider = google-beta
+  project  = data.google_project.project.project_id
 
-  #   depends_on = [
-  #     google_project_service.firebase,
-  #   ]
-# }
+    depends_on = [
+      google_project_service.firebase,
+    ]
+}
 
 
-# resource "google_firestore_database" "database" {
-#   provider                    = google-beta
-#   project                     = var.project_id
-#   location_id                 = var.firestore_location
-#   name                        = "(default)"
-#   type                        = "FIRESTORE_NATIVE"
-#   concurrency_mode            = "OPTIMISTIC"
-#   app_engine_integration_mode = "DISABLED"
+resource "google_firestore_database" "database" {
+  provider                    = google-beta
+  project                     = var.project_id
+  location_id                 = var.firestore_location
+  name                        = "(default)"
+  type                        = "FIRESTORE_NATIVE"
+  concurrency_mode            = "OPTIMISTIC"
+  app_engine_integration_mode = "DISABLED"
 
-#   depends_on = [google_project_service.firestore]
-# }
+  depends_on = [google_project_service.firestore]
+}
 
-# resource "google_firebase_project_location" "default" {
-#   provider    = google-beta
-#   location_id = var.firebase_location
+resource "google_firebase_project_location" "default" {
+  provider    = google-beta
+  location_id = var.firebase_location
 
-#   depends_on = [
-#     google_firebase_project.default,
-#   ]
-# }
+  depends_on = [
+    google_firebase_project.default,
+  ]
+}
 
-# resource "google_firebase_web_app" "basic" {
-#   provider        = google-beta
-#   project         = var.project_id
-#   display_name    = var.project_name
-#   deletion_policy = "DELETE"
+resource "google_firebase_web_app" "basic" {
+  provider        = google-beta
+  project         = var.project_id
+  display_name    = var.project_name
+  deletion_policy = "DELETE"
 
-#   depends_on = [
-#     google_project_service.firebase,
-#     google_firebase_project.default
-#   ]
-# }
+  depends_on = [
+    google_project_service.firebase,
+    google_firebase_project.default
+  ]
+}
 
-# resource "google_storage_bucket" "default" {
-#   provider = google-beta
-#   project  = var.project_id
-#   name     = var.firebase_bucket_name
-#   location = "EU"
-# }
+resource "google_storage_bucket" "default" {
+  provider = google-beta
+  project  = var.project_id
+  name     = var.firebase_bucket_name
+  location = "EU"
+}
 
-# resource "google_firebase_storage_bucket" "default" {
-#   provider  = google-beta
-#   project   = var.project_id
-#   bucket_id = google_storage_bucket.default.id
+resource "google_firebase_storage_bucket" "default" {
+  provider  = google-beta
+  project   = var.project_id
+  bucket_id = google_storage_bucket.default.id
 
-#   depends_on = [
-#     google_project_service.firebase,
-#     google_project_service.firebasestorage,
-#     google_firebase_project.default
-#   ]
-# }
+  depends_on = [
+    google_project_service.firebase,
+    google_project_service.firebasestorage,
+    google_firebase_project.default
+  ]
+}
 
-# data "google_firebase_web_app_config" "basic" {
-#   provider   = google-beta
-#   web_app_id = google_firebase_web_app.basic.app_id
-# }
+data "google_firebase_web_app_config" "basic" {
+  provider   = google-beta
+  web_app_id = google_firebase_web_app.basic.app_id
+}
 
-# resource "google_storage_bucket_object" "default" {
-#   provider = google-beta
-#   bucket   = google_storage_bucket.default.name
-#   name     = "firebase-config.json"
+resource "google_storage_bucket_object" "default" {
+  provider = google-beta
+  bucket   = google_storage_bucket.default.name
+  name     = "firebase-config.json"
 
-#   content = jsonencode({
-#     appId             = google_firebase_web_app.basic.app_id
-#     apiKey            = data.google_firebase_web_app_config.basic.api_key
-#     authDomain        = data.google_firebase_web_app_config.basic.auth_domain
-#     databaseURL       = lookup(data.google_firebase_web_app_config.basic, "database_url", "")
-#     storageBucket     = lookup(data.google_firebase_web_app_config.basic, "storage_bucket", "")
-#     messagingSenderId = lookup(data.google_firebase_web_app_config.basic, "messaging_sender_id", "")
-#     measurementId     = lookup(data.google_firebase_web_app_config.basic, "measurement_id", "")
-#   })
-# }
+  content = jsonencode({
+    appId             = google_firebase_web_app.basic.app_id
+    apiKey            = data.google_firebase_web_app_config.basic.api_key
+    authDomain        = data.google_firebase_web_app_config.basic.auth_domain
+    databaseURL       = lookup(data.google_firebase_web_app_config.basic, "database_url", "")
+    storageBucket     = lookup(data.google_firebase_web_app_config.basic, "storage_bucket", "")
+    messagingSenderId = lookup(data.google_firebase_web_app_config.basic, "messaging_sender_id", "")
+    measurementId     = lookup(data.google_firebase_web_app_config.basic, "measurement_id", "")
+  })
+}
 
