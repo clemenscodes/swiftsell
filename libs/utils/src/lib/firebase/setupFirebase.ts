fa999766d45c72d98852e38a89cf5b1d1bafa707
Clemens Horn <clemens.horn@mni.thm.de>
2023-03-02 17:11:03 +0100

    feat(web): firebase setup, enable secretmanager via terraform

--- libs/utils/src/lib/firebase/setupFirebase.ts
+++ libs/utils/src/lib/firebase/setupFirebase.ts
@@ -2,8 +2,10 @@ import { firebaseConfig } from '@config';
 import { initializeApp, getApp, getApps } from 'firebase/app';
 
 const setupFirebase = () => {
-    if (getApps().length) return getApp();
-    return initializeApp(firebaseConfig);
+    if (typeof window !== undefined) {
+        if (getApps().length) return getApp();
+        return initializeApp(firebaseConfig);
+    }
 };
 
 export const app = setupFirebase();
