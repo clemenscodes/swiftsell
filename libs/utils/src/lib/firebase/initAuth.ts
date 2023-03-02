42fb2f02431a546d0c51bfa514b90794a4744493
Clemens Horn <clemens.horn@mni.thm.de>
2023-03-03 00:44:00 +0100

    refactor(web): modularize init auth

--- libs/utils/src/lib/firebase/initAuth.ts
+++ libs/utils/src/lib/firebase/initAuth.ts
@@ -1,47 +1,48 @@
+import { onLoginRequestError } from './onLoginRequestError';
+import { onLogoutRequestError } from './onLogoutRequestError';
+import { onTokenRefreshError } from './onTokenRefreshError';
+import { onVerifyTokenError } from './onVerifyTokenError';
 import { getFirebaseConfig, siteConfig } from '@config';
-import { init } from 'next-firebase-auth';
+import * as Cookies from 'cookies';
+import { InitConfig, init } from 'next-firebase-auth';
 
 const TWELVE_DAYS_IN_MS = 12 * 60 * 60 * 24 * 1000;
 
-export const initAuth = () => {
-    init({
-        debug: true,
-        authPageURL: '/auth',
-        appPageURL: '/',
-        loginAPIEndpoint: '/api/login',
-        logoutAPIEndpoint: '/api/logout',
-        onLoginRequestError: (err) => {
-            console.error(err);
-        },
-        onLogoutRequestError: (err) => {
-            console.error(err);
-        },
-        // firebaseAuthEmulatorHost: 'localhost:9099',
-        useFirebaseAdminDefaultCredential: true,
-        firebaseClientInitConfig: {
-            ...getFirebaseConfig(),
-        },
-        cookies: {
-            name: siteConfig.name,
-            // Keys are required unless you set `signed` to `false`.
-            // The keys cannot be accessible on the client side.
-            keys: [
-                process.env.COOKIE_SECRET_CURRENT,
-                process.env.COOKIE_SECRET_PREVIOUS,
-            ],
-            httpOnly: true,
-            maxAge: TWELVE_DAYS_IN_MS,
-            overwrite: true,
-            path: '/',
-            sameSite: 'strict',
-            secure: true, // set this to false in local (non-HTTPS) development
-            signed: true,
-        },
-        onVerifyTokenError: (err) => {
-            console.error(err);
-        },
-        onTokenRefreshError: (err) => {
-            console.error(err);
-        },
-    });
+const cookies: Cookies.Option &
+    Cookies.SetOption & {
+        name: string;
+    } = {
+    name: siteConfig.name,
+    // Keys are required unless you set `signed` to `false`.
+    // The keys cannot be accessible on the client side.
+    keys: [
+        process.env.COOKIE_SECRET_CURRENT as string,
+        process.env.COOKIE_SECRET_PREVIOUS as string,
+    ],
+    httpOnly: true,
+    maxAge: TWELVE_DAYS_IN_MS,
+    overwrite: true,
+    path: '/',
+    sameSite: 'strict',
+    secure: true, // set this to false in local (non-HTTPS) development
+    signed: true,
 };
+
+const config: InitConfig = {
+    debug: true,
+    authPageURL: '/auth',
+    appPageURL: '/',
+    loginAPIEndpoint: '/api/login',
+    logoutAPIEndpoint: '/api/logout',
+    useFirebaseAdminDefaultCredential: true,
+    firebaseClientInitConfig: {
+        ...getFirebaseConfig(),
+    },
+    cookies,
+    onLoginRequestError,
+    onLogoutRequestError,
+    onVerifyTokenError,
+    onTokenRefreshError,
+};
+
+export const initAuth = () => init(config);
