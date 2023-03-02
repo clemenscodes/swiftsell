98d30c58331b6aec2a65567ba4f909105796bc9d
Clemens Horn <clemens.horn@mni.thm.de>
2023-03-02 19:17:01 +0100

    fix(web): resource name updated

--- libs/infra/secret/main.tf
+++ libs/infra/secret/main.tf
@@ -8,7 +8,7 @@ resource "google_secret_manager_secret" "secret" {
 }
 
 resource "google_secret_manager_secret_version" "data" {
-  secret      = google_secret_manager_secret.secret-basic.id
+  secret      = google_secret_manager_secret.secret.id
   secret_data = var.secret_data
 }
 
