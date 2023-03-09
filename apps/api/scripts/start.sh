3f839ebd687006e071584811c84655583c751f9b
Clemens Horn <clemens.horn@mni.thm.de>
2023-03-09 19:13:45 +0100

    feat(api): update image name

--- apps/api/scripts/start.sh
+++ apps/api/scripts/start.sh
@@ -1,9 +1,40 @@
 #!/bin/sh
 
 APP="api"
-IMAGE="$APP"
 TAG="sha-$(git rev-parse --short HEAD)"
+REGISTRY="docker.pkg.dev"
+PROJECT="swiftsell-development"
+ARTIFACT_REGION="europe-west1"
+REPO_NAME="docker"
+SERVICE_NAME="api"
 
-INPUT_TAGS=$TAG INPUT_IMAGES=$IMAGE nx docker $APP --skip-nx-cache --configuration=production
+image() {
+    CONFIG="$1"
+    echo "Running in $CONFIG configuration"
+    if [ -z "$SERVICE_NAME" ]; then
+        SERVICE_NAME="api"
+    fi
+    INPUT_IMAGES="$ARTIFACT_REGION-$REGISTRY/$PROJECT/$REPO_NAME/$SERVICE_NAME"
+    if [ -z "$CI" ]; then
+        INPUT_TAGS=$TAG INPUT_IMAGES=$INPUT_IMAGES nx docker $APP --skip-nx-cache --configuration=production
+        IMAGE="$INPUT_IMAGES:$TAG"
+        echo "$IMAGE"
+        docker run --rm -d -p 3000:3000/tcp "$IMAGE"
+    else
+        echo "In CI"
+        # if [ -z "$INPUT_GITHUB_TOKEN" ]; then
+        #     echo "Missing GitHub token"
+        #     exit 1
+        # fi
+        # populate_env_configs "$CONFIG"
+        # NEXT_PUBLIC_PROJECT_TYPE="$CONFIG" nx build "$APP" --skip-nx-cache --configuration="$CONFIG"
+        # INPUT_GITHUB_TOKEN="$INPUT_GITHUB_TOKEN" INPUT_IMAGES="$INPUT_IMAGES" INPUT_TAGS="sha-$SHA" nx docker "$APP" --configuration=ci --skip-nx-cache
+    fi
+    # docker push "$TAGGED_IMAGE"
+}
 
-docker run --rm -d -p 3000:3000/tcp $IMAGE:"$TAG"
+case "$1" in
+development) image "$1" ;;
+production) image "$1" ;;
+*) image "development" ;;
+esac
