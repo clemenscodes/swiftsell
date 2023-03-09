19fe6651fc1a10c1c16d6eae401702133fa85f33
Clemens Horn <clemens.horn@mni.thm.de>
2023-03-09 17:48:45 +0100

    feat(web): nx dev web runs all apps now

--- apps/api/scripts/start.sh
+++ apps/api/scripts/start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-
-IMAGE="nest"
+APP="api"
+IMAGE="$APP"
 TAG="sha-$(git rev-parse --short HEAD)"
 
-INPUT_TAGS=$TAG INPUT_IMAGES=$IMAGE nx docker nest --skip-nx-cache
+INPUT_TAGS=$TAG INPUT_IMAGES=$IMAGE nx docker $APP --skip-nx-cache
 
 docker run --rm -d -p 3000:3000/tcp $IMAGE:"$TAG"
