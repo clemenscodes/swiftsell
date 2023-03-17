76c9d553a1e6d818b657669ffe00798df2ff1706
Clemens Horn <clemens.horn@mni.thm.de>
2023-03-17 10:17:06 +0100

    fix(web): console warnings and console errors resolved

--- libs/shared/src/components/product-list/product-list.spec.tsx
+++ libs/shared/src/components/product-list/product-list.spec.tsx
@@ -1,9 +1,16 @@
 import ProductList from './product-list';
-import { render } from '@testing-library/react';
+import { mockGraphQLClient } from '@test/lib/mockGraphQLClient';
+import { render, act, waitFor } from '@testing-library/react';
+import { Provider } from 'urql';
 
 describe('ProductList', () => {
-    it('should render successfully', () => {
-        // const { baseElement } = render(<ProductList />);
-        // expect(baseElement).toBeTruthy();
+    it('should render successfully', async () => {
+        const wrapper = ({ children }: React.PropsWithChildren) => (
+            <Provider value={mockGraphQLClient}>{children}</Provider>
+        );
+        await act(async () => {
+            const { baseElement } = render(<ProductList />, { wrapper });
+            await waitFor(() => expect(baseElement).toBeTruthy());
+        });
     });
 });
--- libs/shared/src/components/toast/toast.spec.tsx
+++ libs/shared/src/components/toast/toast.spec.tsx
@@ -1,9 +1,10 @@
 import { Toast } from './toast';
+import { ToastProvider } from '@radix-ui/react-toast';
 import { render } from '@testing-library/react';
 
 describe('Toast', () => {
     it('should render successfully', () => {
-        // const { baseElement } = render(<Toast />);
-        // expect(baseElement).toBeTruthy();
+        const { baseElement } = render(<Toast />, { wrapper: ToastProvider });
+        expect(baseElement).toBeTruthy();
     });
 });
